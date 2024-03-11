8d16a3cd4e133f961ae5dff047c30b0b5bbd4698
Parke <parkermauro11@gmail.com>
2024-03-11 22:46:09 +0800

    Exercise 1 pass

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -31,13 +31,7 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
-      int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+      Algorithms.reverse(this.grades);
    }
 
    /**
